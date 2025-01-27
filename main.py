8ff4df6dda7a6cc1278a91a0575af001c0f7a4f2
sshyh <shawn.shyh@powerschool.com>
2025-01-26 23:33:59 -0800

    Add print_bye func

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌃F8 to toggle the breakpoint.
 
+def print_bye(message):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'{message}')  # Press ⌃F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Shawn')
+    print_bye('See ya - Shawn')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
